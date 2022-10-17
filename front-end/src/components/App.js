6bb61a22ed5af1ee7711c6236626edd81acddd63
thaisealves <alves.thaisesilva@gmail.com>
2022-10-17 00:37:05 -0300

    fix: removing hard coded data!

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [student, setStudent] = useState(null);
@@ -10,20 +10,16 @@ function App() {
       const response = await axios.get(`${API_URL}/students/random`);
       const student = response.data;
       if (!student) {
-        alert("Xablau! Não há estudantes cadastrados para o sorteio!");
+        alert('Xablau! Não há estudantes cadastrados para o sorteio!');
       } else {
         setStudent(student);
       }
     } catch (error) {
-      alert("Não foi possível realizar o sorteio!");
+      alert('Não foi possível realizar o sorteio!');
     }
   }, []);
 
-  return student ? (
-    <h1>{/* {student.name} */} Frank Rocha</h1>
-  ) : (
-    "Carregando..."
-  );
+  return student ? <h1>{student.name}</h1> : 'Carregando...';
 }
 
 export default App;
